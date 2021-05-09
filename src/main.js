3d49504aa472122dfe9b2b0e81ee01aca41e4419
Keith Peters <kp@bit-101.com>
2021-05-09 17:41:53 -0400

    image

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-import { Panel, RadioButton, RadioButtonGroup, Checkbox } from "./minimalcomps.js";
+import { Panel, Button, TextInput, Label } from "./minimalcomps.js";
 
-const panel = new Panel(document.body, 40, 40, 400, 310);
-
-for (let i = 0; i < 5; i++) {
-  window["rb" + i] = new RadioButton(panel, 20, 20 + i * 20, "foo", "foo " + i, true, () => {
-  })
-}
+const panel = new Panel(document.body, 40, 40, 200, 200);
+new Button(panel, 20, 20, "Click me", () => label.text = input.text);
+const input = new TextInput(panel, 20, 50, "hello world");
+const label = new Label(panel, 20, 80, "target label");
 
