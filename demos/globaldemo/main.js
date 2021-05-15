2124b082640abce46e9f8010573e03225d6a3007
Keith Peters <kp@bit-101.com>
2021-05-15 12:38:57 -0400

    demo

--- demos/globaldemo/main.js
+++ demos/globaldemo/main.js
@@ -1,4 +1,4 @@
-const panel = new mc2.Panel(document.body, 40, 40, 400, 400);
+const panel = new mc2.Panel(document.body, 40, 40, 580, 400);
 
 new mc2.Button(panel, 20, 20, "Button");
 new mc2.Checkbox(panel, 20, 50, "Checkbox");
@@ -12,9 +12,16 @@ new mc2.TextArea(panel, 20, 240, "TextArea");
 new mc2.HSlider(panel, 60, 370, "HSlider", 40, 0, 100);
 new mc2.VSlider(panel, 165, 230, "VSlider", 40, 0, 100);
 
-new mc2.ColorPicker(panel, 250, 20, "#f00");
-new mc2.NumericStepper(panel, 250, 50, 20, 0, 100);
+new mc2.ColorPicker(panel, 220, 20, "#f00");
+new mc2.NumericStepper(panel, 220, 50, 20, 0, 100);
 const stuff = ["one", "two", "three", "four", "five"];
-new mc2.Dropdown(panel, 250, 80, stuff, 0);
-new mc2.Image(panel, 250, 120, "demo.jpg");
-new mc2.TextBox(panel, 250, 250, "TextBox - a multiline, fixed size text box for displaying non-editable text, including html.").html=true;
+new mc2.Dropdown(panel, 220, 80, stuff, 0);
+new mc2.Image(panel, 220, 120, "demo.jpg");
+new mc2.TextBox(panel, 220, 250, "TextBox - a multiline, fixed size text box for displaying non-editable text, including html.").html=true;
+const canvas = new mc2.Canvas(panel, 350, 20, 200, 100);
+canvas.context.lineWidth = 0.5;
+canvas.context.beginPath();
+for (let i = 0; i < 100; i++) {
+  canvas.context.lineTo(Math.random() * 200, Math.random() * 100);
+}
+canvas.context.stroke();
--- demos/moduledemo/main.js
+++ demos/moduledemo/main.js
@@ -1,6 +1,6 @@
-import { Panel, Button, Checkbox, Label, ProgressBar, RadioButton, TextInput, TextArea, HSlider, VSlider, ColorPicker, NumericStepper, Dropdown, Image, TextBox } from "./minimalcomps.mjs";
+import { Panel, Button, Checkbox, Label, ProgressBar, RadioButton, TextInput, TextArea, HSlider, VSlider, ColorPicker, NumericStepper, Dropdown, Image, TextBox, Canvas } from "./minimalcomps.mjs";
 
-const panel = new Panel(document.body, 40, 40, 400, 400);
+const panel = new Panel(document.body, 40, 40, 580, 400);
 
 new Button(panel, 20, 20, "Button")
 new Checkbox(panel, 20, 50, "Checkbox");
@@ -14,9 +14,16 @@ new TextArea(panel, 20, 240, "TextArea");
 new HSlider(panel, 60, 370, "HSlider", 40, 0, 100);
 new VSlider(panel, 165, 230, "VSlider", 40, 0, 100);
 
-new ColorPicker(panel, 250, 20, "#f00");
-new NumericStepper(panel, 250, 50, 20, 0, 100);
+new ColorPicker(panel, 220, 20, "#f00");
+new NumericStepper(panel, 220, 50, 20, 0, 100);
 const stuff = ["one", "two", "three", "four", "five"];
-new Dropdown(panel, 250, 80, stuff, 0);
-new Image(panel, 250, 120, "demo.jpg");
-new TextBox(panel, 250, 250, "TextBox - a multiline, fixed size text box for displaying non-editable text, including html.").html=true;
+new Dropdown(panel, 220, 80, stuff, 0);
+new Image(panel, 220, 120, "demo.jpg");
+new TextBox(panel, 220, 250, "TextBox - a multiline, fixed size text box for displaying non-editable text, including html.").html=true;
+const canvas = new Canvas(panel, 350, 20, 200, 100);
+canvas.context.lineWidth = 0.5;
+canvas.context.beginPath();
+for (let i = 0; i < 100; i++) {
+  canvas.context.lineTo(Math.random() * 200, Math.random() * 100);
+}
+canvas.context.stroke();
