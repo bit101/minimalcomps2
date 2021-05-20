4f5ec8b16e728cc5f2f964886116cf40e90412bf
Keith Peters <kp@bit-101.com>
2021-05-20 05:16:03 -0400

    object destructure

--- demos/globaldemo/main.js
+++ demos/globaldemo/main.js
@@ -1,24 +1,26 @@
-const panel = new mc2.Panel(document.body, 40, 40, 580, 400);
+const { Panel, Button, Checkbox, RadioButton, Label, ProgressBar, TextInput, TextArea, HSlider, VSlider, ColorPicker, NumericStepper, Dropdown, Image, TextBox, Canvas } = mc2;
 
-new mc2.Button(panel, 20, 20, "Button");
-new mc2.Checkbox(panel, 20, 50, "Checkbox");
-new mc2.RadioButton(panel, 20, 80, "group", "RadioButton 1", true);
-new mc2.RadioButton(panel, 20, 100, "group", "RadioButton 2", false);
-new mc2.RadioButton(panel, 20, 120, "group", "RadioButton 3", false);
-new mc2.Label(panel, 20, 150, "Label");
-new mc2.ProgressBar(panel, 20, 180, 30, 100);
-new mc2.TextInput(panel, 20, 210, "TextInput");
-new mc2.TextArea(panel, 20, 240, "TextArea");
-new mc2.HSlider(panel, 20, 370, "HSlider", 40, 0, 100);
-new mc2.VSlider(panel, 160, 180, "VSlider", 40, 0, 100);
+const panel = new Panel(document.body, 40, 40, 580, 400);
 
-new mc2.ColorPicker(panel, 220, 20, "#f00");
-new mc2.NumericStepper(panel, 220, 50, 20, 0, 100);
+new Button(panel, 20, 20, "Button");
+new Checkbox(panel, 20, 50, "Checkbox");
+new RadioButton(panel, 20, 80, "group", "RadioButton 1", true);
+new RadioButton(panel, 20, 100, "group", "RadioButton 2", false);
+new RadioButton(panel, 20, 120, "group", "RadioButton 3", false);
+new Label(panel, 20, 150, "Label");
+new ProgressBar(panel, 20, 180, 30, 100);
+new TextInput(panel, 20, 210, "TextInput");
+new TextArea(panel, 20, 240, "TextArea");
+new HSlider(panel, 20, 370, "HSlider", 40, 0, 100);
+new VSlider(panel, 160, 180, "VSlider", 40, 0, 100);
+
+new ColorPicker(panel, 220, 20, "#f00");
+new NumericStepper(panel, 220, 50, 20, 0, 100);
 const stuff = ["one", "two", "three", "four", "five"];
-new mc2.Dropdown(panel, 220, 80, stuff, 0);
-new mc2.Image(panel, 220, 120, "demo.jpg");
-new mc2.TextBox(panel, 220, 250, "TextBox - a multiline, fixed size text box for displaying non-editable text, including html.").html=true;
-const canvas = new mc2.Canvas(panel, 350, 20, 200, 100);
+new Dropdown(panel, 220, 80, stuff, 0);
+new Image(panel, 220, 120, "demo.jpg");
+new TextBox(panel, 220, 250, "TextBox - a multiline, fixed size text box for displaying non-editable text, including html.").html=true;
+const canvas = new Canvas(panel, 350, 20, 200, 100);
 canvas.context.lineWidth = 0.5;
 canvas.context.beginPath();
 for (let i = 0; i < 100; i++) {
