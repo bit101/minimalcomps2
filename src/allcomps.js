bb13f0a5fce43b7e8c947f46836913763fc8f60b
Keith Peters <kp@bit-101.com>
2021-05-30 19:37:23 -0400

    v1.3.1 (#52)

--- src/allcomps.js
+++ src/allcomps.js
@@ -25,7 +25,7 @@ import { VBox } from "./vbox.js";
 import { VSlider } from "./vslider.js";
 import { Window } from "./window.js";
 
-const version = "1.3.0";
+const version = "1.3.1";
 
 export {
   version,
